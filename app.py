f4cb9504070f6785a58ad857c3d8bc9012c648a1
axel1536 <axeljeremy200@gmail.com>
2026-01-14 21:02:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import unicodedata
 from typing import Optional, Tuple, Dict, List, Any
-from modules.caja_chica import mostrar_caja_chica
+from caja_chica import mostrar_caja_chica
 
 # ==================== CONFIGURACIÓN DE PÁGINA ====================
 st.set_page_config(
